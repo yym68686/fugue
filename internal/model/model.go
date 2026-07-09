80eb5f38888f7f55b68cf03d27ce2e0dc2a688f1
yym68686 <yym68686@outlook.com>
2026-07-10 00:58:53 +0800

    Bump node updater version for command env fix

--- internal/model/model.go
+++ internal/model/model.go
@@ -110,7 +110,7 @@ const (
 	NodeUpdaterStatusActive  = "active"
 	NodeUpdaterStatusRevoked = "revoked"
 
-	NodeUpdaterCurrentVersion = "v21"
+	NodeUpdaterCurrentVersion = "v22"
 
 	NodeUpdateTaskTypeRefreshJoinConfig           = "refresh-join-config"
 	NodeUpdateTaskTypeUpgradeK3SAgent             = "upgrade-k3s-agent"
